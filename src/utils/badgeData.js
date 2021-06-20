b7cda06933da1abb9436a788fe76307f293d4316
Aman Yadav <42231378+yadav-aman@users.noreply.github.com>
2021-06-20 11:59:45 +0530

    removed redundant exports

--- src/utils/badgeData.js
+++ src/utils/badgeData.js
@@ -1,7 +1,3 @@
-import TurtleBadge from "../images/turtle-badge.svg";
-import FishBadge from "../images/fish-badge.svg";
-import PenguinBadge from "../images/penguin-badge.svg";
-import DolphinBadge from "../images/dolphin-badge.svg";
 import BronzeMedal from "../images/bronze-medal.png";
 import SilverMedal from "../images/silver-medal.png";
 import GoldMedal from "../images/gold-medal.png";
@@ -11,24 +7,24 @@ import DiamondMedal from "../images/diamond-medal.png";
 const badges = [
   {
     src: BronzeMedal,
-    message: " points! You won a bronze medal!"
+    message: " points! You won a bronze medal!",
   },
   {
     src: SilverMedal,
-    message: " points! You won a silver medal!"
+    message: " points! You won a silver medal!",
   },
   {
     src: GoldMedal,
-    message: " points! You won a gold medal!"
+    message: " points! You won a gold medal!",
   },
   {
     src: DoubleStarMedal,
-    message: " points! You won a double star medal!"
+    message: " points! You won a double star medal!",
   },
   {
     src: DiamondMedal,
-    message: " point! You won a diamond medal!"
-  }
+    message: " point! You won a diamond medal!",
+  },
 ];
 
 export default badges;
